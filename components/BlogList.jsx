1bc471502deef12ab30c6c1ce3d24dca232c8d17
Het <hra.opash@gmail.com>
2026-03-25 11:31:43 +0530

    Show a BlogList using Category filter Footer create

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,29 +1,64 @@
+"use client";
 import { blog_data } from "@/Assets/assets";
 import BlogItem from "./BlogItem";
+import { useState } from "react";
 
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
   return (
     <div className="">
       <div className="flex justify-center gap-6 my-10">
-        <button className="bg-black text-white py-1 px-4 rounded-sm">
+        <button
+          onClick={() => setMenu("All")}
+          className={
+            menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
           All
         </button>
-        <button>Technology</button>
-        <button>Startup</button>
-        <button>LifeStyle</button>
+        <button
+          className={
+            menu === "Technology"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+          onClick={() => setMenu("Technology")}
+        >
+          Technology
+        </button>
+        <button
+          className={
+            menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+          onClick={() => setMenu("Startup")}
+        >
+          Startup
+        </button>
+        <button
+          className={
+            menu === "Lifestyle"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+          onClick={() => setMenu("Lifestyle")}
+        >
+          Lifestyle
+        </button>
       </div>
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {blog_data.map((item, index) => {
-          return (
-            <BlogItem
-              key={index}
-              title={item.title}
-              description={item.description}
-              image={item.image}
-              category={item.category}
-            />
-          );
-        })}
+        {blog_data
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => {
+            return (
+              <BlogItem
+                key={index}
+                title={item.title}
+                description={item.description}
+                image={item.image}
+                category={item.category}
+              />
+            );
+          })}
       </div>
     </div>
   );
